7f811e355747651943b2ff2d275187fda473685c
KurylukDEV <KurylukDev@gmail.com>
2025-04-27 21:03:25 -0300

    nuevo arreglo tp

--- Trabajo Practico Mate + Programacion/Simon-Femia-Kuryluk_TP2.py
+++ Trabajo Practico Mate + Programacion/Simon-Femia-Kuryluk_TP2.py
@@ -1,22 +1,22 @@
 def decimal_a_binario(decimal):
-    # Consigna: Convierte un número decimal entero a su representación binaria.
+    # Consigna: Convierte un número decimal entero positivo a su representación binaria.
 
-    if not isinstance(decimal, int) or decimal < 0: # Verificar si la entrada es un entero positivo
-        return None # Retornar None si la entrada no es un entero positivo
+    if decimal < 0: # Verificar si la entrada es un entero positivo
+        return None # Retornar None si la entrada no es un entero positivo y se ingresa letras da error
     if decimal == 0:
         return "0"
     binario = "" # Inicializar la cadena de binario
     while decimal > 0:
-        residuo = decimal % 2 # Obtener el residuo de la division y agregarlo a la cadena
+        residuo = decimal % 2 # Obtener el residuo de la division y agregarlo a la cadena este residuo o es 1 o 0
         binario = str(residuo) + binario # Concatenar el residuo al inicio de la cadena
         decimal //= 2 # Dividir el decimal entre 2
     return binario # Retornar la cadena de binario
 
 # Solicitar la entrada al usuario
-numero_decimal_str = input("Por favor, ingresa un número decimal entero: ")
+numero_decimal_usuario = input("Por favor, ingresa un número decimal entero: ")
 
 # Intentar convertir la entrada a entero (sin validación previa)
-numero_decimal = int(numero_decimal_str)
+numero_decimal = int(numero_decimal_usuario)
 
 # Convertir el número decimal a binario
 numero_binario = decimal_a_binario(numero_decimal)
